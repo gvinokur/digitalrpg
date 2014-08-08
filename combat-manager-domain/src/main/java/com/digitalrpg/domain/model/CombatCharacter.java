44cbaefc8ef5f3f065b2ddba16990d7719b491ce
Gabriel Vinokur <gvinokur@gmail.com>
2014-08-07 21:21:36 -0300

    nit

--- combat-manager-domain/src/main/java/com/digitalrpg/domain/model/CombatCharacter.java
+++ combat-manager-domain/src/main/java/com/digitalrpg/domain/model/CombatCharacter.java
@@ -35,7 +35,7 @@ public abstract class CombatCharacter<ACTION_TYPE extends SystemAction> {
 
     private Boolean hidden;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Combat.class)
     @JoinColumn(name = "combat_id", referencedColumnName = "id")
     @Type(type = "Combat")
     public Combat<ACTION_TYPE> getCombat() {
